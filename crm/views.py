c1d7fd471e765bbcae4935b5b851fc8a281756ab
issokov <issokov@yandex.ru>
2021-03-23 16:41:25 +0300

    Add conrete employee view

--- crm/views.py
+++ crm/views.py
@@ -1,10 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Employee, Aircraft, AircraftDeviceLife
 
 # Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 
 @login_required
@@ -27,3 +27,16 @@ class AircraftsDevicesView(PermissionRequiredMixin, ListView):
 class EmployeesView(LoginRequiredMixin, ListView):
     model = Employee
     template_name = "employees.html"
+
+
+class EmployeeView(LoginRequiredMixin, DetailView):
+    model = Employee
+    template_name = "employee.html"
+
+    def get_object(self, **kwargs):
+        return Employee.objects.get(id=self.kwargs['pk'])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['availability'] = 'Not available'  # Create Employee
+        return context
