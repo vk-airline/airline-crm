115ac7f57c18273fd986308d701816edea0c9b9c
Dmitry Tsykunov <>
2021-03-25 18:31:45 +0300

    add celery example usage

--- crm/urls.py
+++ crm/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from crm.views import index, AircraftView, AircraftsView, AircraftsDevicesView, EmployeesView, EmployeeView
+from crm.views import *
 
 app_name = "crm"
 urlpatterns = [
     path('', index, name='index'),
+    path('blocking/', blocking, name="celery test"),
+    path('nonblocking/', nonblocking, name="celery test2"),
     path('aircrafts', AircraftsView.as_view(), name='aircrafts'),
     path('aircrafts/<int:pk>', AircraftView.as_view(), name='aircraft view'),
     path('aircrafts_devices/', AircraftsDevicesView.as_view(), name='aircrafts devices'),
