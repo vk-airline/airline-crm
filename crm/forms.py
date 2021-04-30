34889892d639c3a52f079465f24a427052aa6d50
issokov <issokov@yandex.ru>
2021-04-30 03:50:03 +0300

    Update flight plan form view

--- crm/forms.py
+++ crm/forms.py
@@ -1,12 +1,50 @@
 from django.forms import ModelForm
 from crm.models import FlightPlan, Flight
 from django import forms
+from multiselectfield import MultiSelectFormField
 
 
 class FlightPlanForm(ModelForm):
     class Meta:
         model = FlightPlan
         fields = "__all__"
+        widgets = {
+            'planning_departure_time': forms.TimeInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'HH:MM:SS',
+                'required': True,
+            }),
+            'planning_arrival_time': forms.TimeInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'HH:MM:SS',
+                'required': True,
+            }),
+            'start_date': forms.DateInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'YYYY-MM-DD',
+                'required': True,
+            }),
+            'end_date': forms.DateInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'YYYY-MM-DD',
+                'required': True,
+            }),
+            'flight_code': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': '**-***',
+                'required': True,
+            }),
+            'passanger_capacity': forms.NumberInput(attrs={
+                'class': 'form-control',
+                'required': True,
+            }),
+            'status': forms.Select(attrs={'class': 'form-control'}),
+            'days_of_week': forms.CheckboxSelectMultiple(attrs={}),
+            "description": forms.Textarea(attrs={'class': 'form-control', 'rows': 5, 'readonly': ''}),
+            'source': forms.Select(attrs={'class': 'form-control'}),
+            'destination': forms.Select(attrs={'class': 'form-control'}),
+
+        }
 
 
 class FlightForm(ModelForm):
